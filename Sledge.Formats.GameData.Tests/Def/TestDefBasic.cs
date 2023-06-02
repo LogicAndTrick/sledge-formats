77bb51b78673e3b2cfb80e3016a29528023664dd
Daniel Walder <daniel.walder@gmail.com>
2023-06-02 17:06:47 +1000

    Half-done def (old qc quake entity format) parser, I can't find a proper spec for this and everyone seems to have a slightly different format so I'm not sure this is worth doing.

---
+++ Sledge.Formats.GameData.Tests/Def/TestDefBasic.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sledge.Formats.GameData.Objects;
+using Sledge.Formats.Precision;
+
+namespace Sledge.Formats.GameData.Tests.Def;
+
+[TestClass]
+public class TestDefBasic
+{
+    private static GameDefinition Parse(string def)
+    {
+        var df = new DefFormat();
+        return df.Read(def);
+    }
+
+    [TestMethod]
+    public void TestEmpty()
+    {
+        var def = Parse("");
+        Assert.AreEqual(0, def.Classes.Count);
+
+        def = Parse("\n\n");
+        Assert.AreEqual(0, def.Classes.Count);
+    }
+
+    [TestMethod]
+    public void TestComments()
+    {
+        const string text = @"//hello";
+        var def = Parse(text);
+        Assert.AreEqual(0, def.Classes.Count);
+    }
+
+    [TestMethod]
+    public void TestBasic()
+    {
+        const string text = @"
+/*QUAKED some_thing
+*/
+";
+        var def = Parse(text);
+        Assert.AreEqual(1, def.Classes.Count);
+
+        var cls = def.Classes[0];
+        Assert.AreEqual("some_thing", cls.Name);
+    }
+
+    [TestMethod]
+    public void TestSolid()
+    {
+        const string text = @"
+/*QUAKED func_solid (0 .1 .2) ? FLAG1 FLAG2
+description
+*/
+";
+        var def = Parse(text);
+        Assert.AreEqual(1, def.Classes.Count);
+
+        var cls = def.Classes[0];
+        Assert.AreEqual("func_solid", cls.Name);
+        Assert.AreEqual("description", cls.Description);
+        Assert.AreEqual(1, cls.Behaviours.Count);
+        
+        var col = cls.Behaviours[0];
+        Assert.AreEqual(Color.FromArgb(255, 0, Convert.ToInt32(0.1d * 255), Convert.ToInt32(0.2d * 255)), col.GetColour(0));
+
+        Assert.AreEqual(1, cls.Properties.Count);
+        var flg = cls.Properties[0];
+        Assert.AreEqual("spawnflags", flg.Name);
+        Assert.AreEqual(2, flg.Options.Count);
+        Assert.AreEqual("1", flg.Options[0].Key);
+        Assert.AreEqual("FLAG1", flg.Options[0].Description);
+        Assert.AreEqual("2", flg.Options[1].Key);
+        Assert.AreEqual("FLAG2", flg.Options[1].Description);
+    }
+
+    [TestMethod]
+    public void TestPoint()
+    {
+        const string text = @"
+/*QUAKED weapon_shotgun (0 .5 .8) (-16 -16 0) (16 16 32)
+{ model("":progs/g_shotgn.mdl""); }
+Shotgun.
+*/
+";
+        var def = Parse(text);
+        Assert.AreEqual(1, def.Classes.Count);
+
+        var cls = def.Classes[0];
+        Assert.AreEqual("weapon_shotgun", cls.Name);
+        Assert.AreEqual("Shotgun.", cls.Description);
+        Assert.AreEqual(2, cls.Behaviours.Count);
+        
+        var col = cls.Behaviours[0];
+        Assert.AreEqual(Color.FromArgb(255, 0, (int)(0.5d * 255), (int)(0.8d * 255)), col.GetColour(0));
+
+        var siz = cls.Behaviours[1];
+        // todo more assertions
+    }
+
+    [TestMethod]
+    public void TestMultiple()
+    {
+        const string text = @"
+/*QUAKED weapon_shotgun (0 .5 .8) (-16 -16 0) (16 16 32)
+{ model("":progs/g_shotgn.mdl""); }
+Shotgun.
+*/
+/*QUAKED func_solid (0 .1 .2) ? FLAG1 FLAG2
+description
+*/
+";
+        var def = Parse(text);
+        Assert.AreEqual(2, def.Classes.Count);
+
+        var cls1 = def.Classes[0];
+        Assert.AreEqual("weapon_shotgun", cls1.Name);
+
+        var cls2 = def.Classes[0];
+        Assert.AreEqual("func_solid", cls2.Name);
+    }
+}
---
+++ Sledge.Formats.GameData/DefFormat.cs
@@ -0,0 +1,213 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using Sledge.Formats.FileSystem;
+using Sledge.Formats.GameData.Objects;
+using Sledge.Formats.Tokens;
+using Sledge.Formats.Tokens.Readers;
+using static Sledge.Formats.Tokens.TokenParsing;
+
+namespace Sledge.Formats.GameData
+{
+    public class DefFormat
+    {
+        private static readonly char[] ValidSymbols = {
+            Symbols.OpenParen,
+            Symbols.CloseParen,
+            Symbols.OpenBrace,
+            Symbols.CloseBrace,
+            Symbols.Semicolon,
+            Symbols.Star,
+            Symbols.Slash,
+            Symbols.Comma,
+            Symbols.Equal,
+            Symbols.Minus,
+            Symbols.Question,
+            Symbols.Dot,
+        };
+
+        private static readonly Tokeniser Tokeniser = new Tokeniser(
+            new SingleLineCommentTokenReader(),
+            new StringTokenReader(),
+            new UnsignedIntegerTokenReader(),
+            new SymbolTokenReader(ValidSymbols),
+            new NameTokenReader()
+        )
+        {
+            EmitWhitespace = true,
+            EmitComments = true
+        };
+
+        public static GameDefinition ReadFile(string path)
+        {
+            var format = new DefFormat();
+            using (var f = File.OpenRead(path))
+            {
+                using (var sr = new StreamReader(f))
+                {
+                    return format.Read(sr);
+                }
+            }
+        }
+
+        public GameDefinition Read(string text)
+        {
+            using (var reader = new StringReader(text))
+            {
+                return Read(reader);
+            }
+        }
+
+        public GameDefinition Read(TextReader reader)
+        {
+            var def = new GameDefinition();
+
+            var tokens = Tokeniser.Tokenise(reader);
+            using (var it = tokens.GetEnumerator())
+            {
+                it.MoveNext();
+                while (it.Current != null && !it.Current.Is(TokenType.End))
+                {
+                    if (it.Current.Is(TokenType.Symbol, "/"))
+                    {
+                        var ent = ParseEntity(it);
+                        def.Classes.Add(ent);
+                    }
+                    else
+                    {
+                        it.MoveNext();
+                    }
+                }
+            }
+
+            return def;
+        }
+
+        private GameDataClass ParseEntity(IEnumerator<Token> it)
+        {
+            Expect(it, TokenType.Symbol, "/");
+            Expect(it, TokenType.Symbol, "*");
+
+            // skip QUAKED string
+            SkipWhile(it, x => !x.Is(TokenType.Whitespace));
+            Expect(it, TokenType.Whitespace);
+
+            var name = Expect(it, TokenType.Name).Value;
+            SkipWhitespace(it);
+            var cls = new GameDataClass(name, "", ClassType.BaseClass);
+
+            if (it.Current?.Is(TokenType.Symbol, "(") == true)
+            {
+                cls.ClassType = ClassType.PointClass;
+                Expect(it, TokenType.Symbol, "(");
+                var rd = ParseDecimal(it);
+                SkipWhitespace(it);
+                var gd = ParseDecimal(it);
+                SkipWhitespace(it);
+                var bd = ParseDecimal(it);
+                SkipWhitespace(it);
+                var r = Convert.ToInt32(rd < 1 ? rd * 255 : rd).ToString(CultureInfo.InvariantCulture);
+                var g = Convert.ToInt32(gd < 1 ? gd * 255 : gd).ToString(CultureInfo.InvariantCulture);
+                var b = Convert.ToInt32(bd < 1 ? bd * 255 : bd).ToString(CultureInfo.InvariantCulture);
+                Expect(it, TokenType.Symbol, ")");
+                SkipWhitespace(it);
+                cls.Behaviours.Add(new Behaviour("color", r, g, b));
+
+                if (it.Current?.Is(TokenType.Symbol, "(") == true)
+                {
+                    Expect(it, TokenType.Symbol, "(");
+                    SkipWhitespace(it);
+                    var x1 = Convert.ToInt32(ParseDecimal(it)).ToString(CultureInfo.InvariantCulture);
+                    SkipWhitespace(it);
+                    var y1 = Convert.ToInt32(ParseDecimal(it)).ToString(CultureInfo.InvariantCulture);
+                    SkipWhitespace(it);
+                    var z1 = Convert.ToInt32(ParseDecimal(it)).ToString(CultureInfo.InvariantCulture);
+                    SkipWhitespace(it);
+                    Expect(it, TokenType.Symbol, ")");
+                    SkipWhitespace(it);
+                    Expect(it, TokenType.Symbol, "(");
+                    SkipWhitespace(it);
+                    var x2 = Convert.ToInt32(ParseDecimal(it)).ToString(CultureInfo.InvariantCulture);
+                    SkipWhitespace(it);
+                    var y2 = Convert.ToInt32(ParseDecimal(it)).ToString(CultureInfo.InvariantCulture);
+                    SkipWhitespace(it);
+                    var z2 = Convert.ToInt32(ParseDecimal(it)).ToString(CultureInfo.InvariantCulture);
+                    SkipWhitespace(it);
+                    Expect(it, TokenType.Symbol, ")");
+                    SkipWhitespace(it);
+
+                    cls.Behaviours.Add(new Behaviour("size", x1, y1, z1, x2, y2, z2));
+                }
+                else if (it.Current?.Is(TokenType.Symbol, "?") == true)
+                {
+                    cls.ClassType = ClassType.SolidClass;
+                    Expect(it, TokenType.Symbol, "?");
+                    SkipWhitespace(it);
+                }
+
+                var flags = new Property("spawnflags", VariableType.Flags, "");
+                var num = 0;
+                while (it.Current != null && !it.Current.Is(TokenType.NewLine))
+                {
+                    var flagToken = ExpectAny(it, TokenType.Name, TokenType.Symbol);
+                    if (flagToken.Is(TokenType.Name))
+                    {
+                        flags.Options.Add(new Option
+                        {
+                            Key = (1u << num).ToString(CultureInfo.InvariantCulture),
+                            Description = flagToken.Value
+                        });
+                    }
+                    else if (flagToken.Value != "-")
+                    {
+                        throw new InvalidOperationException($"Expected -, got {flagToken.Value} instead.");
+                    }
+
+                    num++;
+                    SkipWhitespace(it);
+                }
+
+                if (flags.Options.Count > 0)
+                {
+                    cls.Properties.Add(flags);
+                }
+            }
+
+            Expect(it, TokenType.NewLine);
+            SkipWhitespace(it);
+
+            if (it.Current?.Is(TokenType.Symbol, "{") == true)
+            {
+                // parse properties (TB specific?)
+                throw new NotImplementedException();
+            }
+
+            var desc = "";
+            while (it.Current?.Is(TokenType.End) != true)
+            {
+                if (it.Current?.Is(TokenType.Symbol, "*") == true)
+                {
+                    it.MoveNext();
+                    if (it.Current?.Is(TokenType.Symbol, "/") == true)
+                    {
+                        it.MoveNext();
+                        break;
+                    }
+                    else
+                    {
+                        desc += "*";
+                    }
+                }
+                desc += it.Current?.Value;
+                it.MoveNext();
+            }
+
+            cls.Description = desc.Trim();
+
+            return cls;
+        }
+    }
+}
