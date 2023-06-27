81c4d508d6dbd3b24bb8efa58b36943b55d6f39f
Daniel Walder <daniel.walder@gmail.com>
2023-06-27 20:40:55 +1000

    Add some convenient helper methods

--- Sledge.Formats.Map/Objects/Entity.cs
+++ Sledge.Formats.Map/Objects/Entity.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Numerics;
 
 namespace Sledge.Formats.Map.Objects
 {
@@ -13,6 +16,56 @@ namespace Sledge.Formats.Map.Objects
         {
             SortedProperties = new List<KeyValuePair<string, string>>();
             Properties = new SortedKeyValueDictionaryWrapper(SortedProperties);
+        }
+
+        public int GetIntProperty(string key, int defaultValue) => GetProperty(key, defaultValue);
+        public float GetFloatProperty(string key, float defaultValue) => GetProperty(key, defaultValue);
+        public decimal GetDecimalProperty(string key, decimal defaultValue) => GetProperty(key, defaultValue);
+        public string GetStringProperty(string key, string defaultValue) => GetProperty(key, defaultValue);
+
+        public Vector3 GetVectorProperty(string key, Vector3 defaultValue)
+        {
+            if (!Properties.ContainsKey(key)) return defaultValue;
+
+            var value = Properties[key];
+            if (string.IsNullOrWhiteSpace(value)) return defaultValue;
+
+            var spl = value.Split(' ');
+            if (spl.Length != 3) return defaultValue;
+
+            if (!float.TryParse(spl[0], NumberStyles.Float, CultureInfo.InvariantCulture, out var x)) return defaultValue;
+            if (!float.TryParse(spl[1], NumberStyles.Float, CultureInfo.InvariantCulture, out var y)) return defaultValue;
+            if (!float.TryParse(spl[2], NumberStyles.Float, CultureInfo.InvariantCulture, out var z)) return defaultValue;
+
+            return new Vector3(x, y, z);
+        }
+
+        public Vector4 GetVector4Property(string key, Vector4 defaultValue)
+        {
+            if (!Properties.ContainsKey(key)) return defaultValue;
+
+            var value = Properties[key];
+            if (string.IsNullOrWhiteSpace(value)) return defaultValue;
+
+            var spl = value.Split(' ');
+            if (spl.Length != 4) return defaultValue;
+
+            if (!float.TryParse(spl[0], NumberStyles.Float, CultureInfo.InvariantCulture, out var x)) return defaultValue;
+            if (!float.TryParse(spl[1], NumberStyles.Float, CultureInfo.InvariantCulture, out var y)) return defaultValue;
+            if (!float.TryParse(spl[2], NumberStyles.Float, CultureInfo.InvariantCulture, out var z)) return defaultValue;
+            if (!float.TryParse(spl[3], NumberStyles.Float, CultureInfo.InvariantCulture, out var w)) return defaultValue;
+
+            return new Vector4(x, y, z, w);
+        }
+
+        public T GetProperty<T>(string key, T defaultValue)
+        {
+            if (!Properties.ContainsKey(key)) return defaultValue;
+
+            var value = Properties[key];
+            if (string.IsNullOrWhiteSpace(value)) return defaultValue;
+
+            return (T)Convert.ChangeType(value, typeof(T));
         }
     }
 }
