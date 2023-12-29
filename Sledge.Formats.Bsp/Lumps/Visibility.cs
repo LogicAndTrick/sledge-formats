6a9e8c2dc8a4ada96a8a21a6404697662dff7e9a
Daniel Walder <daniel.walder@gmail.com>
2023-12-29 14:25:29 +1000

    Very basic bsp visibility functionality

--- Sledge.Formats.Bsp/Lumps/Visibility.cs
+++ Sledge.Formats.Bsp/Lumps/Visibility.cs
@@ -1,5 +1,9 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
+using System.Reflection;
+using Sledge.Formats.Bsp.Objects;
 
 namespace Sledge.Formats.Bsp.Lumps
 {
@@ -31,6 +35,37 @@ namespace Sledge.Formats.Bsp.Lumps
         {
             bw.Write(VisData);
             return VisData.Length;
+        }
+
+        public bool IsVisible(Leaves lump, int self, int other)
+        {
+            var leaf = lump[self];
+            return GetVisibleLeaves(lump.Count, leaf).Contains(other);
+        }
+
+        public IEnumerable<int> GetVisibleLeaves(int numLeaves, Leaf leaf)
+        {
+            var offset = leaf.VisOffset;
+            for (var i = 1; i < numLeaves; offset++)
+            {
+                if (VisData[offset] == 0)
+                {
+                    // not visible, next byte is the number of leaves to skip
+                    offset++;
+                    i += 8 * VisData[offset];
+                }
+                else
+                {
+                    // at least 1 of the next 8 leaves is visible, look through each bit to find which one
+                    for (byte bit = 1; bit != 0; bit <<= 1, i++)
+                    {
+                        if ((VisData[offset] & bit) != 0)
+                        {
+                            yield return i;
+                        }
+                    }
+                }
+            }
         }
     }
 }
