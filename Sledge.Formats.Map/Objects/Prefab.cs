fe515c80599e9f35a35368e70a61c43e5819fc17
Daniel Walder <daniel.walder@gmail.com>
2024-12-28 12:12:13 +1000

    Initial support for prefab libraries added to JACK beta version

--- Sledge.Formats.Map/Objects/Prefab.cs
+++ Sledge.Formats.Map/Objects/Prefab.cs
@@ -5,6 +5,7 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public MapFile Map { get; set; }
+        public byte[] PreviewImage { get; set; }
 
         public Prefab()
         {
